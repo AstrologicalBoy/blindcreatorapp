c1a71fb3bd668246f263c19fffc5f0c231f4557f
Astrological Boy <57241112+AstrologicalBoy@users.noreply.github.com>
2025-04-05 12:07:48 -0400

    Update loading.tsx

--- loading.tsx
+++ loading.tsx
@@ -2,7 +2,7 @@ import { GridLoader } from 'react-spinners'
 
 const loading = () => {
   return (
-    <div className="h-dvh flex justify-center items-center">
+    <div className="min-h-dvh flex justify-center items-center">
         <GridLoader color={'#bc15f4'} />
     </div>
   )
